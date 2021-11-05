9f1eef0a259f02645499273d58823a332d26b7f6
Arkyb <Arkybarky83838@outlook.com>
2021-11-05 19:32:38 -0400

    zooming through, finished task 3, however, using vs code i can never see my outputs because of code that was already written (like instructions) i need to use codepen to view if things work but i dont have time yet so these are all done without work checking

--- index.js
+++ index.js
@@ -208,17 +208,19 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log(artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
-
+console.log(artists[2].bio)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
 
-
+//I uh, I dont know if you want this coded or not because im tempted to go back and just change the typo, idk if this is like, a trick question or not but uh, yeah.
+artists[9].name = "Vincent Van Gogh";
+console.log(artists[9].name);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -228,10 +230,10 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, number) {
+  return `The artist at index ${array[number]} is ${array[number].name}`;
 }
-
+console.log(getArtistByIndex(artists, 4));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
